83e828b978da098d5e37e3f5fd4d1e571689c36d
sodapng <sodapng@ya.ru>
2022-06-11 02:37:43 +0400

    fix: whitespace path

--- src/handlers/line.js
+++ src/handlers/line.js
@@ -1,12 +1,22 @@
+import { delimiter } from 'node:path'
+
 export default function handleLine(eventEmitter, line) {
   try {
-    const [command, ...args] = line.split(' ')
+    let [command, ...args] = line.split(' ')
+
+    if (/"|'/g.test(args)) {
+      args = args
+        .join(' ')
+        .split('" ')
+        .map((arg) => arg.replace(/"|'/g, ''))
+    }
+
     if (/^(?:cd|cat|add|rm|os|hash)$/.test(command)) {
       if (args.length !== 1) throw new Error('Invalid input')
       eventEmitter.emit(command, args)
     } else if (/^(?:rn|cp|mv|compress|decompress)$/.test(command)) {
       if (args.length !== 2) throw new Error('Invalid input')
-      eventEmitter.emit(command)
+      eventEmitter.emit(command, args)
     } else if (/^(?:up|ls)$/.test(line)) {
       eventEmitter.emit(command)
     } else if (/^\.exit$/.test(command)) {
